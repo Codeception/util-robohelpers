622a3065d7c0d853223522c2f560cd90ab6a6da0
Gintautas Miselis <gintautas@miselis.lt>
2019-12-18 11:51:15 +0200

    Updated deployment instructions

--- src/DocumentationHelpers.php
+++ src/DocumentationHelpers.php
@@ -27,9 +27,21 @@ trait DocumentationHelpers
 # $moduleName
 ## Installation
 
-This module was bundled with Codeception 2 and 3, but since version 4 it is necessary to install it separately 
-by running `composer require --dev codeception/$packageName` in project directory 
-or enabling `$moduleName` module in suite configuration file and running `codecept init upgrade4`.
+If you use Codeception installed using composer, install this module with the following command:
+
+```
+composer require --dev codeception/$packageName
+```
+
+Alternatively, you can enable `$moduleName` module in suite configuration file and run
+ 
+```
+codecept init upgrade4.
+```
+
+This module was bundled with Codeception 2 and 3, but since version 4 it is necessary to install it separately.   
+Some modules are bundled with PHAR files.  
+Warning. Using PHAR file and composer in the same project can cause unexpected errors.  
 
 ## Description
 
